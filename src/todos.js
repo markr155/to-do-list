96d94b33b7e4eaadd408915b2701791fe461662f
markr155 <markricho155@gmail.com>
2023-09-15 21:24:29 +1000

    we in there

--- src/todos.js
+++ src/todos.js
@@ -5,11 +5,10 @@
 //     Priority - Low Medium High
 //     Completed
 const toDoFactory = (title, description, dueDate, priority) => {
-    const getTitle = () => title;
-    return { getTitle, description, dueDate, priority}
+    return { title, description, dueDate, priority}
 };
 
 const toDo1 = toDoFactory('Code', 'Finish to-do-list project', 'Next week', 'High');
 
-console.log(toDo1.getTitle());
+console.log(toDo1.title);
 
