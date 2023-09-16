4d7195f6b94f787b22d42d4e196f050bc206c52c
markr155 <markricho155@gmail.com>
2023-09-16 21:54:10 +1000

    main header done

--- src/domChanges.js
+++ src/domChanges.js
@@ -1,6 +1,8 @@
 // Add ToDo-s to main div
 function displayToDos(toDoArray) {
-    const main = document.querySelector('.main');
+    // Clear Main
+    main.textContent = '';
+    main.appendChild(mainHeader());
     // Loop through To Do Array and display each item 
     toDoArray.forEach((toDo, index) => {
         const newToDo = document.createElement('div');
@@ -16,11 +18,33 @@ function displayToDos(toDoArray) {
     });
 }
 
+// Returns the item property as a div
 function addPropertyToDisplay(item, property) {
     const element = document.createElement('div');
     element.classList.add(property);
     element.textContent = item[property];
     return element;
 }
+
+// Creates header in main div
+function mainHeader() {
+    const header = document.createElement('div');
+    const headerTitle = document.createElement('div');
+    headerTitle.textContent = 'Title';
+    const headerDescription = document.createElement('div');
+    headerDescription.textContent = 'Description';
+    const headerDueDate = document.createElement('div');
+    headerDueDate.textContent = 'Due Date';
+    const headerPriority = document.createElement('div');
+    headerPriority.textContent = 'Priority';
+    const headerCompleted = document.createElement('div');
+    headerCompleted.textContent = 'Completed';
+    header.appendChild(headerTitle);
+    header.appendChild(headerDescription);
+    header.appendChild(headerDueDate);
+    header.appendChild(headerPriority);
+    header.appendChild(headerCompleted);
+    return header;
+};
 
 export default displayToDos;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import defaultData from './todos.js';
 import displayToDos from './domChanges.js';
 
+//element declaration
+const main = document.getElementById('main');
 
 displayToDos(defaultData());
